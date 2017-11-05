0058ebb97520729aac6142cd81f094b3a92b220c
Selva Sanjeevi Julian <sel@apple.com>
2017-11-05 06:52:43 -0800

    Login Control Module

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -2,24 +2,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Toggle from "./Toggle"
+import LoginControl from "./LoginControl"
 
-var run = setInterval(tick, 1000);
+class Clock extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
 
-ReactDOM.render(<NestedFunctionalComponent/>, document.getElementById("content4"));
-ReactDOM.render(<App />, document.getElementById("content"));
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>
-                The time is {props.date.toLocaleTimeString()}
-            </h1>
-        </div>
-    );
-}
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
-function tick() {
-    ReactDOM.render(<Clock date={new Date()}/>, document.getElementById("content3")); 
+    tick() {
+        this.setState( {
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    The time is {this.state.date.toLocaleTimeString()}
+                </h1>
+            </div>
+        );    
+    }
 }
 
 function NestedFunctionalComponent() {
@@ -34,3 +50,9 @@ function NestedFunctionalComponent() {
 function NestedComp(props) {
     return <h1>Welcome {props.name}</h1>;
 }
+
+ReactDOM.render(<App />, document.getElementById("content"));
+ReactDOM.render(<Clock />, document.getElementById("content3")); 
+ReactDOM.render(<NestedFunctionalComponent/>, document.getElementById("content4"));
+ReactDOM.render(<Toggle />, document.getElementById("content5"));
+// ReactDOM.render(<LoginControl />, document.getElementById("greeting"));
